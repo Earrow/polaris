f20d46655743ef233319ac19f3eb4df3b01394e6
Earrow <hongcliu@icloud.com>
2018-05-28 14:38:15 +0800

    结果统计协议说明

--- app/report_result.py
+++ app/report_result.py
@@ -6,6 +6,24 @@ from functools import wraps
 
 
 def report(host, project_name, task_name):
+    """将测试结果上报给平台，测试结果由用户统计并将统计函数注册在该函数下。
+
+    :param host: polaris平台地址。
+    :param project_name: 项目名。
+    :param task_name: 测试任务名。
+    :return: 测试用例数量、出错数量、失败数量、跳过执行数量组成的元组。
+
+    示例：
+    >>> @report('127.0.0.1:5000', 'Test', 't')
+    ... def result_statistics():
+    ...    tests = 5
+    ...    errors = 1
+    ...    failures = 1
+    ...    skip = 0
+    ...    return tests, errors, failures, skip
+
+    >>> result_statistics()  # 执行统计脚本的命令需在创建任务时的结果统计栏填写
+    """
     url = 'http://{}/records/report_result'.format(host)
 
     def out_wrapper(func):
@@ -28,12 +46,3 @@ def report(host, project_name, task_name):
         return wrapper
 
     return out_wrapper
-
-
-if __name__ == '__main__':
-    @report('127.0.0.1:5000', 'intent', 't')
-    def fun():
-        return 1, 2, 3, 4
-
-    fun()
-
